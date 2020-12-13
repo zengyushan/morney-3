ecd578203ac83b8800c7b13e1c050a7cd902b3bc
zengyushan <zyszys805@139.com>
2020-12-13 15:51:23 +0800

    新增editLabel

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -1,10 +1,26 @@
+import EditLabel from '@/views/EditLabel.vue';
 <template>
     <Layout></Layout>
 </template>
 
 <script lang="ts">
-export default {
-    
+import { tagListModel } from "@/models/tagListModel";
+import Vue from "vue";
+import { Component } from 'vue-property-decorator';
+
+@Component
+export default class EditLabel extends Vue{
+    created(){
+        const id = this.$route.params.id;
+        tagListModel.fetch();
+        const tags = tagListModel.data;
+        const tag = tags.filter(t => t.id)[0];
+        if(tag){
+            console.log(tag);
+        }else{
+            this.$router.replace("/404");
+        }
+    }
 }
 </script>
 <style lang="scss">
