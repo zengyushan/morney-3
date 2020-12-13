807aa3e9e35ba550b1f4ba8033fc0701e7ba4a6f
zengyushan <zyszys805@139.com>
2020-12-13 15:56:33 +0800

    点击标签可跳转编辑页面

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,11 +1,11 @@
 <template>
   <layout>
-    <ol class="tags">
-      <li class="tag" v-for="tag in tags" :key="tag.id">
+    <div class="tags">
+      <router-link class="tag" v-for="tag in tags" :key="tag.id" :to="`/labels/edit/${tag.id}`">
         <span>{{tag.name}}</span>
         <Icon name="right" />
-      </li>
-    </ol>
+      </router-link>
+    </div>
     <div class="createTag-wrapper">
       <button class="createTag" @click="createTag">新建标签</button>
     </div>
